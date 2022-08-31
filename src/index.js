1c017658de077f2d35b5934435993a389843f20d
Danny <77531150+kitten17@users.noreply.github.com>
2022-08-31 18:53:02 +0400

    hash router + package.json updates are included

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import App from './App';
 
 import './firebase'
 
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
 );
