0c5fc08af42a20b2a60458d0e192a4843a70b668
kitten20 <sml.iz@mail.ru>
2023-03-10 00:57:58 +0400

    Test - valentine + icon

--- src/pages/Valentine/index.jsx
+++ src/pages/Valentine/index.jsx
@@ -1,29 +1,39 @@
 import { useRef, useState } from "react";
+import { Helmet } from "react-helmet";
 
 import ValentineContainer from "./ValentineContainer";
 import ValentinePasswordInput from "./ValentinePasswordInput";
 
 import module from "./style.module.scss";
 
+import heartIcon from './assets/heart-icon.png';
+
 function Valentine() {
   const [passwordBoolean, setPasswordInputBoolean] = useState(true);
   const valentineContainerRef = useRef(null);
 
   return (
-    <div className={module.valentine}>
-      <div
-        className={module["valentine-container"]}
-        ref={valentineContainerRef}
-        onLoad={() =>
-          valentineContainerRef.current.classList.add(module.active)
-        }
-      >
-        <ValentinePasswordInput
-          {...{ passwordBoolean, setPasswordInputBoolean }}
-        />
-        <ValentineContainer />
+    <>
+      <Helmet>
+        <title>Это тебе, моя хорошая...</title>
+        <link rel="icon" href={heartIcon}></link>
+      </Helmet>
+
+      <div className={module.valentine}>
+        <div
+          className={module["valentine-container"]}
+          ref={valentineContainerRef}
+          onLoad={() =>
+            valentineContainerRef.current.classList.add(module.active)
+          }
+        >
+          <ValentinePasswordInput
+            {...{ passwordBoolean, setPasswordInputBoolean }}
+          />
+          <ValentineContainer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
